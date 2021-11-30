57d74bbb2998023da6f3e0b93a75219aaa8a4787
test <test@192.168.1.6>
2021-11-30 11:31:54 +0100

    first commit

---
+++ perksapiauth.py
@@ -0,0 +1,18 @@
+def auth_req():
+    import requests
+    import json
+
+    url = "https://test3.perksdev.com/api/v1/Authorize"
+
+    payload = json.dumps({
+      "email": "psst@tsst.com",
+      "password": "Test123!"
+    })
+    headers = {
+      'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return print(response.text)
+    
+auth_req()
---
+++ perksauthstapi.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+url = "https://test3.perksdev.com/api/v1/Authorize"
+
+payload = json.dumps({
+  "email": "psst@tsst33.com",
+  "password": "Test123!"
+})
+headers = {
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
